9f57379df3bd2f2cb229ddce68c93a282a113b31
cssutavani91 <53695690+cssutavani91@users.noreply.github.com>
2019-08-26 00:46:04 +0530

    Add files via upload

---
+++ 03-MessageBox/MessageBox.cpp
@@ -0,0 +1,108 @@
+//Headers
+#include<Windows.h>
+
+//Global Function Declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Winmain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR ipszCmdLine, int iCmdShow)
+{
+	// Varible Declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//Code
+	//Initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//Create Window
+	hwnd = CreateWindow(szAppName, TEXT("My Application"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//Message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//code
+	/*HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rc;
+	TCHAR Str[255] = TEXT("Hello World !!!");*/
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		MessageBox(hwnd, TEXT("WM_CREATE message is sent"), TEXT("Message"), MB_OK);
+		break;
+
+	case WM_MOVE:
+		MessageBox(hwnd, TEXT("WM_MOVE message is sent"), TEXT("Message"), MB_OK);
+		break;
+
+	case WM_SIZE:
+		MessageBox(hwnd, TEXT("WM_SIZE message is sent"), TEXT("Message"), MB_OK);
+		break;
+
+	case WM_KEYDOWN:
+		MessageBox(hwnd, TEXT("WM_KEYDOWN message is sent"), TEXT("Message"), MB_OK);
+		break;
+
+	case WM_LBUTTONDOWN:
+		MessageBox(hwnd, TEXT("WM_LBUTTONDOWN message is sent"), TEXT("Message"), MB_OK);
+		break;
+
+	/*case WM_PAINT:
+		//Get changing rectangle
+		GetClientRect(hwnd, &rc);
+		//Call the specialist i.e Create HDC
+		hdc = BeginPaint(hwnd, &ps);
+		//Set the text colour
+		SetTextColor(hdc, RGB(0, 255, 0));
+		//Set Background Color
+		SetBkColor(hdc, RGB(0, 0, 0));
+		//Draw the actual text
+		DrawText(hdc, Str, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		//Send back the specialist and stop painting
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+		}
+		break;*/
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
