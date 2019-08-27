66497fd4a6a3bf0947d0d9b1ae67686eb4c53817
cssutavani91 <53695690+cssutavani91@users.noreply.github.com>
2019-08-27 19:28:48 +0530

    Add files via upload

---
+++ DLL/1) DLL Server/1) DeclSpec/MyMath/MyMath.cpp
@@ -0,0 +1,40 @@
+#include<Windows.h>
+
+//extern "C" BOOL IsNoNegative(int);
+
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec (dllexport) int MakeSquare(int num)
+{
+	/*HWND hwnd;
+//	int i;
+	TCHAR Str[255];
+	//i = IsNoNegative(num);
+	if (i == FALSE)
+	{ 
+		wsprintf(Str, TEXT("The Number is negative"));
+		MessageBox(hwnd, Str, TEXT("Message"), MB_OK);*/
+	return (num*num);
+}
+
+/*extern "C" BOOL IsNoNegative(int num)
+{
+	if (num < 0)
+		return TRUE;
+	else
+		return FALSE;
+}*/
